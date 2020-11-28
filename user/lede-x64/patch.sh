6a22583ffbeb7c930382af9f7925f0216e53b840
alecthw <alecthw@163.com>
2020-11-28 10:41:42 +0800

    update

--- user/lede-x64/patch.sh
+++ user/lede-x64/patch.sh
@@ -10,17 +10,16 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-a
 rm -rf package/lean/luci-app-guest-wifi
 #rm -rf package/lean/luci-app-kodexplorer
 rm -rf package/lean/luci-app-pppoe-relay
-#rm -rf package/lean/luci-app-ramfree
+rm -rf package/lean/luci-app-ramfree
 rm -rf package/lean/luci-app-softethervpn
 
 # xiaorouji obsolete
-rm -rf package/lean/luci-app-v2ray-server
 #rm -rf package/lean/luci-app-verysync
 
 # xiaorouji package
 #rm -rf package/lean/dns2socks
 #rm -rf package/lean/ipt2socks
-#rm -rf package/lean/kcptun
+rm -rf package/lean/kcptun
 rm -rf package/lean/microsocks
 #rm -rf package/lean/pdnsd-alt
 #rm -rf package/lean/shadowsocksr-libev
