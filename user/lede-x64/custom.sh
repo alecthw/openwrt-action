6734cb0d2e9111ca7a6767ea4ea84b512882baf5
alecthw <alecthw@163.com>
2021-04-19 18:59:04 +0800

    update

--- user/lede-x64/custom.sh
+++ user/lede-x64/custom.sh
@@ -7,7 +7,7 @@ rm -rf feeds/packages/net/smartdns
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns feeds/packages/net/smartdns
 
 rm -rf feeds/packages/libs/libcap
-svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap
+svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap feeds/packages/libs/libcap
 
 # clean default config of https-dns-proxy
 cat >package/feeds/packages/https-dns-proxy/files/https-dns-proxy.config <<EOF
