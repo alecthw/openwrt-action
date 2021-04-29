6df5770ff7919c7816582cdd62d0b0698de51177
alecthw <alecthw@163.com>
2021-04-29 15:25:21 +0800

    update

--- user/lede-x64/custom.sh
+++ user/lede-x64/custom.sh
@@ -9,10 +9,4 @@ svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns feeds/packa
 rm -rf feeds/packages/libs/libcap
 svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap feeds/packages/libs/libcap
 
-# clean default config of https-dns-proxy
-cat >package/feeds/packages/https-dns-proxy/files/https-dns-proxy.config <<EOF
-config main 'config'
-	option update_dnsmasq_config ''
-EOF
-
 cat package/lean/default-settings/files/zzz-default-settings
