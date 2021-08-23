ac557a5677dfaf6842e662891750c0bc9a169c9b
idn <idn@idn.com>
2021-08-23 18:40:25 +0800

    update

--- user/common/patch.sh
+++ user/common/patch.sh
@@ -12,6 +12,7 @@ echo "Source: ${source}"
 do_common() {
     # add custom packages
     git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/luci-theme-argon-jerrykuku
+    svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-webrestriction package/luci-app-control-webrestriction
 }
 
 do_lienol_common() {
