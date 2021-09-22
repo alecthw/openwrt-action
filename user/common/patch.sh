637cecbfc041ca69c08b6a53bec146d691943042
idn <idn@idn.com>
2021-09-22 15:02:10 +0800

    update

--- user/common/patch.sh
+++ user/common/patch.sh
@@ -11,24 +11,26 @@ echo "Source: ${source}"
 
 do_common() {
     # add custom packages
-    git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/luci-theme-argon-jerrykuku
     git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
     git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 }
 
 do_lienol_common() {
     # add custom packages
+    git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/luci-theme-argon-jerrykuku
     svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-ramfree package/luci-app-ramfree
 }
 
 do_lede_common() {
     # add custom packages
+    git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/luci-theme-argon-jerrykuku
     svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-tcpdump package/luci-app-tcpdump
 
     # remove pkg
     rm -rf package/lean/luci-app-n2n_v2
     rm -rf package/lean/n2n_v2
     rm -rf package/lean/luci-app-jd-dailybonus
+    rm -rf package/lean/luci-app-serverchan
 }
 
 # excute begin
