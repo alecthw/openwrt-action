df086051b3a727de482d1a6e204941b5b710adee
alecthw <alecthw@163.com>
2023-03-14 11:34:23 +0800

    add mosdns

--- user/lede-x64/custom.sh
+++ user/lede-x64/custom.sh
@@ -5,6 +5,12 @@
 echo "Execute custom custom.sh"
 
 # change packages
+rm -rf feeds/luci/applications/luci-app-mosdns
+svn co https://github.com/QiuSimons/openwrt-mos/trunk/luci-app-mosdns feeds/luci/applications/luci-app-mosdns
+
+rm -rf feeds/packages/net/mosdns
+svn co https://github.com/QiuSimons/openwrt-mos/trunk/mosdns feeds/packages/net/mosdns
+
 rm -rf feeds/packages/net/smartdns
 svn co https://github.com/Lienol/openwrt-packages/branches/master/net/smartdns feeds/packages/net/smartdns
 
